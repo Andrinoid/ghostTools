34766746f3230ec55a6a0420b97e35a249099686
Andri Birgisson <andri@tweecode.com>
2016-10-25 23:01:14 +0000

    loader simplified and improved

--- js/dist/loader.js
+++ js/dist/loader.js
@@ -14,73 +14,46 @@ var Loader = function () {
     /**
      * ------------------------------------------------------------------------
      * Constants
+     *
      * ------------------------------------------------------------------------
      */
-    var STYLES = '\n         .simpleLoader {\n             position: fixed;\n             top: 0;\n             left: 0;\n             right: 0;\n             bottom: 0;\n             background: rgba(0, 0, 0, 0.36);\n             z-index: 9999;\n         }\n         .simpleLoader .sl-container {\n             position: absolute;\n             top: 50%;\n             left: 50%;\n             -webkit-transform: translate(-50%, -50%);\n             transform: translate(-50%, -50%);\n         }\n         .simpleLoader .sl-dot {\n             width: 20px;\n             height: 20px;\n             border: 2px solid white;\n             border-radius: 50%;\n             float: left;\n             margin: 0 5px;\n             -webkit-transform: scale(0);\n             transform: scale(0);\n             -webkit-animation: fx 1000ms ease infinite 0ms;\n             animation: fx 1000ms ease infinite 0ms;\n         }\n         .simpleLoader .sl-dot:nth-child(2) {\n             -webkit-animation: fx 1000ms ease infinite 300ms;\n             animation: fx 1000ms ease infinite 300ms;\n         }\n         .simpleLoader .sl-dot:nth-child(3) {\n             -webkit-animation: fx 1000ms ease infinite 600ms;\n             animation: fx 1000ms ease infinite 600ms;\n         }\n         @-webkit-keyframes fx {\n             50% {\n                 -webkit-transform: scale(1);\n                 transform: scale(1);\n                 opacity: 1;\n             }\n             100% {\n                 opacity: 0;\n             }\n         }\n         @keyframes fx {\n             50% {\n                 -webkit-transform: scale(1);\n                 transform: scale(1);\n                 opacity: 1;\n             }\n             100% {\n                 opacity: 0;\n             }\n         }\n     ';
+    var Templates = [];
+    Templates.push('\n      <div class="sl-container">\n         <style scoped>\n         .simpleLoader {\n             position: fixed;\n             top: 0;\n             left: 0;\n             right: 0;\n             bottom: 0;\n             background: rgba(0, 0, 0, 0.36);\n             z-index: 9999;\n         }\n         .simpleLoader .sl-container {\n             position: absolute;\n             top: 50%;\n             left: 50%;\n             -webkit-transform: translate(-50%, -50%);\n             transform: translate(-50%, -50%);\n         }\n         .simpleLoader .sl-dot {\n             width: 20px;\n             height: 20px;\n             border: 2px solid white;\n             border-radius: 50%;\n             float: left;\n             margin: 0 5px;\n             -webkit-transform: scale(0);\n             transform: scale(0);\n             -webkit-animation: fx 1000ms ease infinite 0ms;\n             animation: fx 1000ms ease infinite 0ms;\n         }\n         .simpleLoader .sl-dot:nth-child(2) {\n             -webkit-animation: fx 1000ms ease infinite 300ms;\n             animation: fx 1000ms ease infinite 300ms;\n         }\n         .simpleLoader .sl-dot:nth-child(3) {\n             -webkit-animation: fx 1000ms ease infinite 600ms;\n             animation: fx 1000ms ease infinite 600ms;\n         }\n         @-webkit-keyframes fx {\n             50% {\n                 -webkit-transform: scale(1);\n                 transform: scale(1);\n                 opacity: 1;\n             }\n             100% {\n                 opacity: 0;\n             }\n         }\n         @keyframes fx {\n             50% {\n                 -webkit-transform: scale(1);\n                 transform: scale(1);\n                 opacity: 1;\n             }\n             100% {\n                 opacity: 0;\n             }\n         }\n         </style>\n         <div class="sl-dot"></div>\n         <div class="sl-dot"></div>\n         <div class="sl-dot"></div>\n       </div>\n     ');
+
+    // Material design circular activity spinner
+    // This is the built in chrome spinner and might fail in other browsers
+    Templates.push('\n    <svg version="1" xmlns="http://www.w3.org/2000/svg"\n                     xmlns:xlink="http://www.w3.org/1999/xlink"\n         width="25px" height="25px" viewBox="0 0 16 16">\n        <style scoped>\n          .simpleLoader {\n              position: absolute;\n              left: 50%;\n              top: 50%;\n              transform: translate(-50%, -50%);\n              z-index: 1000;\n          }\n          .qp-circular-loader {\n            width:16px;  /* 2*RADIUS + STROKEWIDTH */\n            height:16px; /* 2*RADIUS + STROKEWIDTH */\n          }\n          .qp-circular-loader-path {\n            stroke-dasharray: 32.4;  /* 2*RADIUS*PI * ARCSIZE/360 */\n            stroke-dashoffset: 32.4; /* 2*RADIUS*PI * ARCSIZE/360 */\n                                     /* hides things initially */\n          }\n          /* SVG elements seem to have a different default origin */\n          .qp-circular-loader, .qp-circular-loader * {\n            -webkit-transform-origin: 50% 50%;\n          }\n          /* Rotating the whole thing */\n          @-webkit-keyframes rotate {\n            from {transform: rotate(0deg);}\n            to {transform: rotate(360deg);}\n          }\n          .qp-circular-loader {\n            -webkit-animation-name: rotate;\n            -webkit-animation-duration: 1568.63ms; /* 360 * ARCTIME / (ARCSTARTROT + (360-ARCSIZE)) */\n            -webkit-animation-iteration-count: infinite;\n            -webkit-animation-timing-function: linear;\n          }\n          /* Filling and unfilling the arc */\n          @-webkit-keyframes fillunfill {\n            from {\n              stroke-dashoffset: 32.3 /* 2*RADIUS*PI * ARCSIZE/360 - 0.1 */\n                                      /* 0.1 a bit of a magic constant here */\n            }\n            50% {\n              stroke-dashoffset: 0;\n            }\n            to {\n              stroke-dashoffset: -31.9 /* -(2*RADIUS*PI * ARCSIZE/360 - 0.5) */\n                                       /* 0.5 a bit of a magic constant here */\n            }\n          }\n          @-webkit-keyframes rot {\n            from {\n              transform: rotate(0deg);\n            }\n            to {\n              transform: rotate(-360deg);\n            }\n          }\n          @-webkit-keyframes colors {\n            from {\n              stroke: #4285f4;\n            }\n            to {\n              stroke: #4285f4;\n            }\n          }\n          .qp-circular-loader-path {\n            -webkit-animation-name: fillunfill, rot, colors;\n            -webkit-animation-duration: 1333ms, 5332ms, 5332ms; /* ARCTIME, 4*ARCTIME, 4*ARCTIME */\n            -webkit-animation-iteration-count: infinite, infinite, infinite;\n            -webkit-animation-timing-function: cubic-bezier(0.4, 0.0, 0.2, 1), steps(4), linear;\n            -webkit-animation-play-state: running, running, running;\n            -webkit-animation-fill-mode: forwards;\n          }\n        </style>\n        <!-- 2.25= STROKEWIDTH -->\n        <!-- 8 = RADIUS + STROKEWIDTH/2 -->\n        <!-- 6.875= RADIUS -->\n        <!-- 1.125=  STROKEWIDTH/2 -->\n        <g class="qp-circular-loader">\n        <path class="qp-circular-loader-path" fill="none"\n              d="M 8,1.125 A 6.875,6.875 0 1 1 1.125,8" stroke-width="2.25"\n              stroke-linecap="round"></path>\n        </g>\n        </svg>\n    ');
 
     var Default = {
-        removeDelay: 0
+        template: '0', // 0 or 1
+        parent: document.body
     };
-
-    var Template = '\n         <div class="sl-container">\n             <div class="sl-dot"></div>\n             <div class="sl-dot"></div>\n             <div class="sl-dot"></div>\n         </div>\n     ';
 
     var Loader = function () {
         function Loader(options) {
             _classCallCheck(this, Loader);
 
+            this.__proto__.removeAll();
+            this.__proto__.instances.push(this);
             this.defaults = Utils.extend(Default, options);
-            this.injectStyles();
             this.createDOM();
         }
-        // getters
 
         _createClass(Loader, [{
-            key: 'injectStyles',
-            value: function injectStyles() {
-                //if styles exists do nothing
-                if (document.getElementById('loaderStyles')) return;
-                var tag = document.createElement('style');
-                tag.type = 'text/css';
-                tag.id = 'loaderStyles';
-                if (tag.styleSheet) {
-                    tag.styleSheet.cssText = STYLES;
-                } else {
-                    tag.appendChild(document.createTextNode(STYLES));
-                }
-                document.getElementsByTagName('head')[0].appendChild(tag);
-            }
-        }, {
             key: 'createDOM',
             value: function createDOM() {
                 var wrapper = document.createElement('div');
                 wrapper.className = 'simpleLoader';
-                wrapper.innerHTML = Template;
-                document.body.appendChild(wrapper);
+                wrapper.innerHTML = Templates[this.defaults.template];
+                this.defaults.parent.appendChild(wrapper);
             }
         }, {
             key: 'remove',
             value: function remove() {
-                setTimeout(function () {
-                    var loaders = document.querySelectorAll('.simpleLoader');
-                    for (var i = 0; i < loaders.length; i++) {
-                        loaders[i].parentNode.removeChild(loaders[i]);
-                    }
-                }, this.defaults.removeDelay);
-            }
-        }, {
-            key: 'STYLES',
-            get: function get() {
-                return STYLES;
-            }
-        }, {
-            key: 'Template',
-            get: function get() {
-                return Template;
-            }
-        }], [{
-            key: 'Default',
-            get: function get() {
-                return Default;
+                var loaders = document.querySelectorAll('.simpleLoader');
+                for (var i = 0; i < loaders.length; i++) {
+                    loaders[i].parentNode.removeChild(loaders[i]);
+                }
             }
         }]);
 
@@ -89,3 +62,10 @@ var Loader = function () {
 
     return Loader;
 }();
+Loader.prototype.instances = [];
+Loader.prototype.removeAll = function () {
+    this.instances.forEach(function (item) {
+        item.remove();
+    });
+    this.instances.length = 0;
+};
--- js/src/loader.js
+++ js/src/loader.js
@@ -9,9 +9,13 @@ const Loader = (() => {
     /**
      * ------------------------------------------------------------------------
      * Constants
+     *
      * ------------------------------------------------------------------------
      */
-    const STYLES = `
+    const Templates = [];
+    Templates.push(`
+      <div class="sl-container">
+         <style scoped>
          .simpleLoader {
              position: fixed;
              top: 0;
@@ -68,72 +72,137 @@ const Loader = (() => {
                  opacity: 0;
              }
          }
-     `;
+         </style>
+         <div class="sl-dot"></div>
+         <div class="sl-dot"></div>
+         <div class="sl-dot"></div>
+       </div>
+     `);
+
+    // Material design circular activity spinner
+    // This is the built in chrome spinner and might fail in other browsers
+    Templates.push(`
+    <svg version="1" xmlns="http://www.w3.org/2000/svg"
+                     xmlns:xlink="http://www.w3.org/1999/xlink"
+         width="25px" height="25px" viewBox="0 0 16 16">
+        <style scoped>
+          .simpleLoader {
+              position: absolute;
+              left: 50%;
+              top: 50%;
+              transform: translate(-50%, -50%);
+              z-index: 1000;
+          }
+          .qp-circular-loader {
+            width:16px;  /* 2*RADIUS + STROKEWIDTH */
+            height:16px; /* 2*RADIUS + STROKEWIDTH */
+          }
+          .qp-circular-loader-path {
+            stroke-dasharray: 32.4;  /* 2*RADIUS*PI * ARCSIZE/360 */
+            stroke-dashoffset: 32.4; /* 2*RADIUS*PI * ARCSIZE/360 */
+                                     /* hides things initially */
+          }
+          /* SVG elements seem to have a different default origin */
+          .qp-circular-loader, .qp-circular-loader * {
+            -webkit-transform-origin: 50% 50%;
+          }
+          /* Rotating the whole thing */
+          @-webkit-keyframes rotate {
+            from {transform: rotate(0deg);}
+            to {transform: rotate(360deg);}
+          }
+          .qp-circular-loader {
+            -webkit-animation-name: rotate;
+            -webkit-animation-duration: 1568.63ms; /* 360 * ARCTIME / (ARCSTARTROT + (360-ARCSIZE)) */
+            -webkit-animation-iteration-count: infinite;
+            -webkit-animation-timing-function: linear;
+          }
+          /* Filling and unfilling the arc */
+          @-webkit-keyframes fillunfill {
+            from {
+              stroke-dashoffset: 32.3 /* 2*RADIUS*PI * ARCSIZE/360 - 0.1 */
+                                      /* 0.1 a bit of a magic constant here */
+            }
+            50% {
+              stroke-dashoffset: 0;
+            }
+            to {
+              stroke-dashoffset: -31.9 /* -(2*RADIUS*PI * ARCSIZE/360 - 0.5) */
+                                       /* 0.5 a bit of a magic constant here */
+            }
+          }
+          @-webkit-keyframes rot {
+            from {
+              transform: rotate(0deg);
+            }
+            to {
+              transform: rotate(-360deg);
+            }
+          }
+          @-webkit-keyframes colors {
+            from {
+              stroke: #4285f4;
+            }
+            to {
+              stroke: #4285f4;
+            }
+          }
+          .qp-circular-loader-path {
+            -webkit-animation-name: fillunfill, rot, colors;
+            -webkit-animation-duration: 1333ms, 5332ms, 5332ms; /* ARCTIME, 4*ARCTIME, 4*ARCTIME */
+            -webkit-animation-iteration-count: infinite, infinite, infinite;
+            -webkit-animation-timing-function: cubic-bezier(0.4, 0.0, 0.2, 1), steps(4), linear;
+            -webkit-animation-play-state: running, running, running;
+            -webkit-animation-fill-mode: forwards;
+          }
+        </style>
+        <!-- 2.25= STROKEWIDTH -->
+        <!-- 8 = RADIUS + STROKEWIDTH/2 -->
+        <!-- 6.875= RADIUS -->
+        <!-- 1.125=  STROKEWIDTH/2 -->
+        <g class="qp-circular-loader">
+        <path class="qp-circular-loader-path" fill="none"
+              d="M 8,1.125 A 6.875,6.875 0 1 1 1.125,8" stroke-width="2.25"
+              stroke-linecap="round"></path>
+        </g>
+        </svg>
+    `);
 
     const Default = {
-        removeDelay: 0
+        template: '0', // 0 or 1
+        parent: document.body
     };
-
-    const Template = `
-         <div class="sl-container">
-             <div class="sl-dot"></div>
-             <div class="sl-dot"></div>
-             <div class="sl-dot"></div>
-         </div>
-     `;
 
     class Loader {
         constructor(options) {
+            this.__proto__.removeAll();
+            this.__proto__.instances.push(this);
             this.defaults = Utils.extend(Default, options);
-            this.injectStyles();
             this.createDOM();
-        }
-            // getters
-
-        static get Default() {
-            return Default
-        }
-
-        get STYLES() {
-            return STYLES
-        }
-
-        get Template() {
-            return Template;
-        }
-
-        injectStyles() {
-            //if styles exists do nothing
-            if (document.getElementById('loaderStyles')) return;
-            var tag = document.createElement('style');
-            tag.type = 'text/css';
-            tag.id = 'loaderStyles';
-            if (tag.styleSheet) {
-                tag.styleSheet.cssText = STYLES;
-            } else {
-                tag.appendChild(document.createTextNode(STYLES));
-            }
-            document.getElementsByTagName('head')[0].appendChild(tag);
         }
 
         createDOM() {
             let wrapper = document.createElement('div');
             wrapper.className = 'simpleLoader';
-            wrapper.innerHTML = Template;
-            document.body.appendChild(wrapper);
+            wrapper.innerHTML = Templates[this.defaults.template];
+            this.defaults.parent.appendChild(wrapper);
         }
 
         remove() {
-            setTimeout(()=> {
-                var loaders = document.querySelectorAll('.simpleLoader');
-                for (var i = 0; i < loaders.length; i++) {
-                    loaders[i].parentNode.removeChild(loaders[i]);
-                }
-            }, this.defaults.removeDelay);
-
+            var loaders = document.querySelectorAll('.simpleLoader');
+            for (var i = 0; i < loaders.length; i++) {
+                loaders[i].parentNode.removeChild(loaders[i]);
+            }
         }
     }
     return Loader;
 })();
+Loader.prototype.instances = [];
+Loader.prototype.removeAll = function() {
+    this.instances.forEach(function(item) {
+        item.remove();
+    });
+    this.instances.length = 0;
+};
 
 export default Loader;
