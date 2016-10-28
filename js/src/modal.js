2a15c40806b9c14b3e7c44a36fbe2eb67853ca4c
Andri Birgisson <andri@tweecode.com>
2016-10-28 11:50:30 +0000

    modal styles

--- js/src/modal.js
+++ js/src/modal.js
@@ -83,8 +83,6 @@ const Modalstyles = `
              position: relative;
              background-color: #fff;
              background-clip: padding-box;
-             border: 1px solid #999;
-             border: 1px solid rgba(0, 0, 0, .2);
              border-radius: 2px;
              outline: 0;
              box-shadow: 0 3px 9px rgba(0, 0, 0, .5)
